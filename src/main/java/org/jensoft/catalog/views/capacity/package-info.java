9cebca4359b72fe46bcc8af3355a3959084a7fe6
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-14 10:21:53 +0200

    add capacity samples

---
+++ src/main/java/org/jensoft/catalog/views/capacity/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides capacity demo views
+ * 
+ * @since 1.0
+ */
+@UIUnit(name = "Capacity", desc = "show capacity cell demos")
+package org.jensoft.catalog.views.capacity;
+
+import org.jensoft.core.catalog.nature.UIUnit;
+
