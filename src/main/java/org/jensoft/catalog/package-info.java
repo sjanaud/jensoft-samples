2d65f2038ddbcd7edc6c24df295cfedaab08c2fd
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-21 07:43:45 +0200

    format

--- src/main/java/org/jensoft/catalog/package-info.java
+++ src/main/java/org/jensoft/catalog/package-info.java
@@ -4,11 +4,11 @@
  * @author Sébastien Janaud
  */
 @org.jensoft.core.catalog.nature.Catalog(name="JenSoftAPI Samples",
-											group="com.jensoft",
-											artifact="jensoft-samples-catalog",
-											version="2.0.5-SNAPSHOT",
-											description="Charts assets for developers to learn how to use JenSoft API",
-											core="2.0.5-SNAPSHOT")
+										group="org.jensoft",
+										artifact="jensoft-samples",
+										version="2.0.5-SNAPSHOT",
+										description="Charts assets for developers to learn how to use JenSoft API",
+										core="2.0.5-SNAPSHOT")
 @org.jensoft.core.catalog.nature.New(comment="Catalog 2 by JenSoftAPI")
 package org.jensoft.catalog;
 
